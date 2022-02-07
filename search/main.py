5c95ad35cb21707cfd02300d5d0fcc1ca6e88b22
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2022-02-07 23:26:40 +0530

    added loop and random for search element

--- search/main.py
+++ search/main.py
@@ -5,7 +5,12 @@ Main function for search algorithms graph plotting
 """
 
 import search
+from random import randint
 import matplotlib.pyplot as plt
+
+
+ARRAY_LENGTH = 10000000 # Array size from 0 to length
+                        # Array of this size will take a lot of memory 
 
 
 def plot_graph(retime, fetime, bitime, jutime, search_element):
@@ -20,51 +25,33 @@ def plot_graph(retime, fetime, bitime, jutime, search_element):
     plt.ylabel('Time in milliseconds')
 
 
-
 def main():
     
+    array = [i for i in range(0, ARRAY_LENGTH)]
+    print('-array created')
+    itr = 4 # Number of times loop will go
+    search_element = 0
+    search_obj = search.search(array, search_element)
+    print('Starting loop')
     
-    array = [i for i in range(0,10000000)]
-    
-    search_element = 9999920
-    search1 = search.search(array, search_element)
+    while itr>0:
+        
+        search_obj.ele = randint(0, ARRAY_LENGTH)
+        print(f'\n-Search element for this iteration is {search_obj.ele}')
+        
+        linearTime, linear = search_obj.linear()
+        print('Search element {} time for execution in linear search {}'.format(linear,linearTime))
+        interpolationTime, interpolation = search_obj.interpolation()
+        print('Search element {} time for execution in interpolation search {}'.format(interpolation,interpolationTime))
+        binaryTime, binary = search_obj.binary()
+        print('Search element {} time for execution in binary search {}'.format(binary,binaryTime))
+        jumpTime, jump = search_obj.jump()
+        print('Search element {} time for execution in jump search {}'.format(jump,jumpTime))
 
-    linearTime, linear = search1.linear()
-    interpolationTime, interpolation = search1.interpolation()
-    binaryTime, binary = search1.binary()
-    jumpTime, jump = search1.jump()
-
-    del search1
-    
-    print('Search element {} time for execution in linear search {}'.format(linear,linearTime))
-    print('Search element {} time for execution in interpolation search {}'.format(interpolation,interpolationTime))
-    print('Search element {} time for execution in binary search {}'.format(binary,binaryTime))
-    print('Search element {} time for execution in jump search {}'.format(jump,jumpTime))
-
-    plot_graph(linearTime, interpolationTime, binaryTime, jumpTime, search_element)
-    
-    
-    # Second array 
-    
-    array = [i for i in range(0,10000)]
-    
-    search_element = 825
-    search2 = search.search(array, search_element)
-
-    linearTime, linear = search2.linear()
-    interpolationTime, interpolation = search2.interpolation()
-    binaryTime, binary = search2.binary()
-    jumpTime, jump = search2.jump()
-    
-    del search2
-    
-    print('\nSearch element {} time for execution in linear search {}'.format(linear,linearTime))
-    print('Search element {} time for execution in interpolation search {}'.format(interpolation,interpolationTime))
-    print('Search element {} time for execution in binary search {}'.format(binary,binaryTime))
-    print('Search element {} time for execution in jump search {}'.format(jump,jumpTime))
-
-    plot_graph(linearTime, interpolationTime, binaryTime, jumpTime, search_element)
-    
+        
+        plot_graph(linearTime, interpolationTime, binaryTime, jumpTime, search_obj.ele)
+        
+        itr -= 1
     
     plt.legend(loc='upper right')
     plt.show()

43513d7a6f2d2d24eaf22cf90a5ce13e368f7d0a
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2022-02-20 00:09:04 +0530

    ds/tree

---
+++ data structures/tree.py
@@ -0,0 +1,27 @@
+
+
+class _tree:
+    
+    
+    def __init__(self, data):
+        self.data = data
+        self.children  = []
+        self.parent = None
+        
+        
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+    
+    
+#    def print_tree(self):
+        
+    
+    
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
